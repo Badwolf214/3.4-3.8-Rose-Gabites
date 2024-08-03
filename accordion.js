acf043b3cf4dbeec90825fc094b08c079fb5e4ed
Rose-Gabites <70927310+Badwolf214@users.noreply.github.com>
2024-08-03 08:46:18 +0000

    Debugging 3/8/24
    Add more here later, created new layout but then it broke randomly, so I'm saving this and rolling back the changes so I can fix it.

--- accordion.js
+++ accordion.js
@@ -1,14 +1,65 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+const quickViewButtons = document.querySelectorAll('[data-quick-view]');
+const closeButtons = document.querySelectorAll('[data-close]');
+const fullwidthCards = document.querySelectorAll('.fullwidth');
+let toggle; // Quick view <button>
+let toggleParent;
+let toggleOpen;// <li>.
+let fullwidth; // Fullwidth card to be "injected".
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
+const openQuickView = (toggle, toggleParent, fullwidth) => {
+    toggle.setAttribute('aria-expanded', 'true');
+    toggleParent.classList.toggle('is-selected');
+    fullwidth.classList.toggle('is-hidden');
+    // Make fullwidth card keyboard focusable.
+    fullwidth.setAttribute('tabIndex', '0');
+};
+
+const closeQuickView = (toggle, toggleParent, fullwidth) => {
+    toggle.setAttribute('aria-expanded', 'false');
+    toggleParent.classList.toggle('is-selected');
+    fullwidth.classList.toggle('is-hidden');
+    fullwidth.removeAttribute('tabIndex');
+};
+
+quickViewButtons.forEach(quickView => {
+    // Add appropriate ARIA attributes for "toggle" behaviour.
+    fullwidth = quickView.parentElement.nextElementSibling;
+    quickView.setAttribute('aria-expanded', 'false');
+    quickView.setAttribute('aria-controls', fullwidth.id);
+
+    quickView.addEventListener('click', (e) => {
+        toggle = e.target;
+        toggleParent = toggle.parentElement;
+        fullwidth = toggleParent.nextElementSibling;
+
+        // Open (or close) fullwidth card.
+        if (toggle.getAttribute('aria-expanded') === 'false') {
+            // Do we have another fullwidth card already open? If so, close it.
+            fullwidthCards.forEach(fullwidthOpen => {
+                if (!fullwidthOpen.classList.contains('is-hidden')) {
+                    toggleParent = fullwidthOpen.previousElementSibling;
+                    toggleOpen = toggleParent.querySelector(
+                        '[data-quick-view]'
+                    );
+
+                    closeQuickView(toggleOpen, toggleParent, fullwidthOpen);
+                }
+            });
+
+            openQuickView(toggle, toggleParent, fullwidth);
+        } else {
+            closeQuickView(toggle, toggleParent, fullwidth);
+        }
+    });
+});
+
+closeButtons.forEach(close => {
+    close.addEventListener('click', (e) => {
+        fullwidth = e.target.parentElement;
+        toggleParent = e.target.parentElement.previousElementSibling;
+        toggle = toggleParent.querySelector('[data-quick-view]');
+
+        closeQuickView(toggle, toggleParent, fullwidth);
+        toggle.focus(); // Return keyboard focus to "toggle" button.
+    });
+});
